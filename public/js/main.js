bf65cea0800eca1356a029a30330e7531798d8e6
skfro266 <skfro266@naver.com>
2018-12-13 17:25:39 +0900

    20181213_02

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,38 @@
-$(".choice_list > li").hover(function(){
-    $(this).css({"box-shadow":"4px 4px 5px rgba(0,0,0,0.3)"});
+var scTop= 0;
+var navChk= true;
+
+$(window).scroll(function(){
+    scTop = $(window).scrollTop();
+    if(scTop > 200){
+        $(".navs").css({"background-color":"#fff"});
+    }
+    else{
+        $(".navs").css({"background-color":"rgba(255,255,255,0.2)"});
+    }
+});
+
+$(".menu > li").hover(function(){
+    $(".drop_menu").stop().fadeOut(0);
+    $(this).find(".drop_menu").stop().fadeIn(100)
 },function(){
+    $(".drop_menu").stop().fadeOut(0);
+});
+
+$(".choice_list > li").mouseenter(function(){
+    $(this).css({"box-shadow":"4px 4px 5px rgba(0,0,0,0.3)"});
+});
+$(".choice_list > li").mouseleave(function(){
     $(this).css({"box-shadow":"none"});
 });
 
-$(".hole_list > li img").hover(function(){
-    $(this).css({"animation-name":"holeAni"});
-},function(){
-    $(this).css({"animation-name":"holeBack"});
+$(".black_bg").mouseenter(function(){
+    $(this).children("img").css({"animation-name":"holeAni"});
+    $(this).children(".hole_bt").show();
 });
+
+$(".black_bg").mouseleave(function(){
+    $(this).children("img").css({"animation-name":"holeBack"});
+    $(this).children(".hole_bt").hide();
+});
+
+var page = new SpaAni(".page",".anis",850);
